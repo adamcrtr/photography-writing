ccf8b5fa8cf167c5bb9bd263c70118a067b71cff
adamcrtr <adampc123@hotmail.co.uk>
2021-06-26 22:33:00 +0100

    Tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,9 @@ module.exports = {
   siteMetadata: {
     title: "Photography and Writing",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
